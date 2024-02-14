ea20809db22b7b23dadeff80c3d171854f87afc6
feedbaak-lab <info@feedbaak.com>
2024-02-14 10:50:01 -0800

    1049

--- index.js
+++ index.js
@@ -4,48 +4,6 @@ require('dotenv').config();
 // Import required libraries
 const express = require('express');
 const mongoose = require('mongoose');
-const Applicant = require('./models/applicant'); // Import the applicant model
-
-// Initialize Express application
-const app = express();
-
-// Set the port to listen on, defaulting to 3000 if not provided in environment variables
-const PORT = process.env.PORT || 3000;
-
-// Disable strict mode for MongoDB queries
-mongoose.set('strictQuery', false);
-
-// Function to connect to MongoDB database
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI); // Connect to MongoDB using the URI from environment variables
-        console.log(`MongoDB Connected: ${conn.connection.host}`); // Log successful connection
-    } catch (error) {
-        console.log(error); // Log any errors that occur during connection
-        process.exit(1); // Exit the process with an error code
-    }
-}
-
-// Define routes and start server after successful database connection
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`); // Log the port the server is listening on
-    })
-});
-
-
-// Route to retrieve all applicants from the database
-app.get('/applicants', async(req,res)=> {
-    const applicant = await Applicant.find(); // Find all applicants in the database
-
-    if(applicant){
-        res.json(applicant) // Send the retrieved applicants as a JSON response
-    } else{
-        res.send("Something went wrong."); // Send an error message if something goes wrong during retrieval
-    }
-
-});
-
 
 // Import the Mongoose schema for the applicant
 const { ApplicantSchema } = require('./models/applicant');
@@ -55,6 +13,9 @@ const { categoryEnum, locationEnum } = require('./enums');
 
 // Create a Mongoose model for the applicant schema
 const Applicant = mongoose.model('Applicant', ApplicantSchema);
+
+// Initialize Express application
+const app = express();
 
 // Connect to MongoDB database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -115,9 +76,7 @@ app.post('/add-applicant', async (req, res) => {
 });
 
 // Start the Express server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
-
